c8753e793a2c794594dcc752847b0f17f5efed7e
eric <ericolsonnv@gmail.com>
2023-02-26 18:46:38 -0800

    Remove player related configs from spotify

--- spotify/config.go
+++ spotify/config.go
@@ -1,40 +1,33 @@
 package spotify
 
 import (
-	"github.com/rs/zerolog/log"
 	"os"
 	"path/filepath"
 	"runtime"
+
+	"github.com/rs/zerolog/log"
 )
 
-type PlayerConfig struct {
-	// CacheSize sets the max file cache size in MB. File caching is disabled if 0.
-	// Defaults to 0.
-	CacheSize int `json:"cache_size"`
-
+type SessionConfig struct {
 	// ConfigHomeDir sets the parent directory for all configs & state files. CacheDir is nested inside this path if
 	// CacheDir is left unset.
 	// Defaults to ${HOME}/.apollo/spotify/ on linux/macos and %userprofile%\AppData\local\apollo\spotify\ on windows.
 	ConfigHomeDir string `json:"config_home_dir"`
 
+	// CacheSize sets the max file cache size in MB. File caching is disabled if 0.
+	// Defaults to 0.
+	CacheSize int `json:"cache_size"`
+
 	// CacheDir sets the directory to be used for cache files. Is only used when CacheSize > 0.
 	// Defaults to ${HOME}/.apollo/spotify/cache/ on linux/macos and %userprofile%\AppData\local\apollo\spotify\cache\ on windows.
 	CacheDir string `json:"cache_dir"`
-
-	// BitRate sets the bit rate of the downloaded files. This can be 96, 160, or 320.
-	// Defaults to 160.
-	BitRate int `json:"bit_rate"`
-
-	// PreloadSize sets the number of enqueued songs to download ahead of time (stored in memory if CacheSize == 0)
-	// Defaults to 2.
-	PreloadSize int `json:"preload_size"`
 
 	// OAuthCallback sets the callback address for oauth logins
 	// Defaults to "" (http://localhost:8888/callback).
 	OAuthCallback string `json:"oauth_callback"`
 }
 
-func DefaultPlayerConfig() PlayerConfig {
+func DefaultSessionConfig() SessionConfig {
 	configHomeDir := ""
 
 	switch runtime.GOOS {
@@ -44,7 +37,7 @@ func DefaultPlayerConfig() PlayerConfig {
 		}
 	default:
 		if path, ok := os.LookupEnv("HOME"); ok {
-			configHomeDir = filepath.Join(path, ".local", ".apollo", "spotify")
+			configHomeDir = filepath.Join(path, ".local", "apollo", "spotify")
 		}
 	}
 
@@ -53,12 +46,10 @@ func DefaultPlayerConfig() PlayerConfig {
 		log.Warn().Msg("could not parse home directory for cache, using ./ instead")
 	}
 
-	return PlayerConfig{
-		CacheSize:     0,
+	return SessionConfig{
 		ConfigHomeDir: configHomeDir,
+		CacheSize:     0,
 		CacheDir:      filepath.Join(configHomeDir, "cache"),
-		BitRate:       160,
-		PreloadSize:   2,
 		OAuthCallback: "",
 	}
 }
