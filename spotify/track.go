fc39848d4e3ea307ba0faf6dd3338f9f435be4a3
eric <ericolsonnv@gmail.com>
2023-02-26 18:48:03 -0800

    Update track to implement the new Playable interface

--- spotify/track.go
+++ spotify/track.go
@@ -3,16 +3,17 @@ package spotify
 import (
 	"encoding/hex"
 	"fmt"
+	"io"
+	"time"
+
 	"github.com/eolso/librespot-golang/Spotify"
+	"github.com/eolso/librespot-golang/librespot/player"
 	"github.com/eolso/librespot-golang/librespot/utils"
 )
 
 type Track struct {
 	spotifyTrack *Spotify.Track
-}
-
-func (t *Track) Id() string {
-	return utils.ConvertTo62(t.spotifyTrack.GetGid())
+	player       *player.Player
 }
 
 func (t *Track) Name() string {
@@ -25,6 +26,18 @@ func (t *Track) Artist() string {
 	}
 
 	return t.spotifyTrack.Artist[0].GetName()
+}
+
+func (t *Track) Metadata() map[string]string {
+	return nil
+}
+
+func (t *Track) Id() string {
+	return utils.ConvertTo62(t.spotifyTrack.GetGid())
+}
+
+func (t *Track) Description() string {
+	return fmt.Sprintf("%s by %s", t.Name(), t.Artist())
 }
 
 func (t *Track) Album() string {
@@ -43,6 +56,51 @@ func (t *Track) Image() string {
 	return ""
 }
 
-func (t *Track) Duration() int32 {
-	return t.spotifyTrack.GetDuration()
+func (t *Track) Duration() time.Duration {
+	return time.Duration(t.spotifyTrack.GetDuration()) * time.Millisecond
+}
+
+func (t *Track) Type() string {
+	return "spotify track"
+}
+
+func (t *Track) Download() (io.ReadCloser, error) {
+	var selectedFile *Spotify.AudioFile
+
+	audioFiles := t.spotifyTrack.GetFile()
+
+	// If the track returned no audio files, try the alternatives until files are found
+	if len(audioFiles) == 0 {
+		for _, alternative := range t.spotifyTrack.Alternative {
+			audioFiles = alternative.GetFile()
+			if len(audioFiles) > 0 {
+				break
+			}
+		}
+	}
+
+	// All alternatives tried, still no files
+	if len(audioFiles) == 0 {
+		return nil, fmt.Errorf("failed to fetch track data %s", t.Id())
+	}
+
+	// Try and grab a desired codec first
+	for _, file := range t.spotifyTrack.GetFile() {
+		for _, codec := range targetCodecs {
+			if file.GetFormat() == codec {
+				selectedFile = file
+				break
+			}
+		}
+		if selectedFile != nil {
+			break
+		}
+	}
+
+	// Grab whatever is left
+	if selectedFile == nil {
+		selectedFile = audioFiles[0]
+	}
+
+	return t.player.LoadTrack(selectedFile, t.spotifyTrack.GetGid())
 }
