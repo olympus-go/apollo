973e24f3ea003e14765bcf4e92fe7ca701f8f181
eric <ericolsonnv@gmail.com>
2023-05-06 13:19:09 -0700

    Mo errors mo problems

--- spotify/errors.go
+++ spotify/errors.go
@@ -1,6 +1,11 @@
 package spotify
 
-import "errors"
+import (
+	"errors"
+)
 
+var ErrPlayerQueueFull = errors.New("player queue is full")
+var ErrPlayerCommandTimeout = errors.New("player command timed out")
+var ErrTokenNotFound = errors.New("spotify: auth token not found")
 var ErrPlayerAlreadyLoggedIn = errors.New("spotify: player already logged in")
 var ErrEmptySearchResponse = errors.New("spotify: search yielded no results")
